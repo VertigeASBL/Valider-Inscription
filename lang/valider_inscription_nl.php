453e632ae116c7f4e9ee1a220927d0c33a0057b6
Michel Bystranowski <michel@vertige.org>
2016-04-08 10:27:55 +0200

    ajoute une traduction en néerlandais

---
+++ lang/valider_inscription_nl.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'cfg_exemple' => 'Bij voorbeeld ',
+	'cfg_exemple_explication' => 'Uitleg van deze voorbeeld',
+	'cfg_titre_parametrages' => 'Parameters instellen',
+	'erreur_validation' => 'Een is een fout ontstaan tijdens de validatie',
+	'info_nouveau' => 'Te valideren',
+	'info_valider' => 'Er wordt automatisch een email verstuurd naar de bezoeker teneinde zijn identifier te verschaffen',
+	'message_inscription_ok' => 'Uw verkrijgt uw identifier via email eenmaal uw inschrijvingsaanvraag gevalideerd wordt door een e-administrator',
+	'notification_inscription_a_valider_titre' => 'Nieuwe inschrijving te valideren op @nom_site@',
+	'notification_inscription_a_valider_texte' => 'Een nieuwe inschrijving werd op de site @naam_site@.Afspraak @lien_auteur@ geregistreerd teneinde ter validatie',
+	'permissions_insuffisantes' => 'U hebt de toestemming niet om dit de doen ',
+	'texte_notification_inscription_a_valider' => 'administrator melden wanneer een nieuwe inschrijving op de site werd geregistreed',
+	'titre_notification_inscription_a_valider' => 'Nieuwe inschrijving te valideren  ',
+	'titre_page_configurer_valider_inscription' => 'Inschrijvingen valideren',
+	'valider_inscription_titre' => 'Inschrijvingen valideren',
+	'valider_membre' => 'De inschrijving van deze auteur valideren',
+);
