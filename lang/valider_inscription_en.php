21738ff4bee32763f61e11dd7f8ae2d14b633819
Debondt Didier <p@henix.be>
2017-01-04 14:50:36 +0100

    Anglais :)

---
+++ lang/valider_inscription_en.php
@@ -0,0 +1,36 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'cfg_exemple' => 'Exemple',
+	'cfg_exemple_explication' => 'Explication de cet exemple',
+	'cfg_titre_parametrages' => 'Paramétrages',
+
+	// E
+	'erreur_validation' => 'An error occurred while validating',
+
+	// I
+	'info_nouveau' => 'to be validated',
+	'info_valider' => 'An email will be automatically sent to the visitor with his login and password',
+
+	// M
+	'message_inscription_ok' => 'Your login will be sent to you by e-mail once your application has been validated by an administrator.',
+
+	// N
+	'notification_inscription_a_valider_titre' => 'New registration to validate on @nom_site@',
+	'notification_inscription_a_valider_texte' => 'A new registration has been registered on the site @nom_site@. Go to @lien_auteur@ to validate it!',
+
+	// P
+	'permissions_insuffisantes' => 'You do not have permission to do this',
+
+	// T
+	'texte_notification_inscription_a_valider' => 'Notify administrators when a new registration is registered on the site',
+	'titre_notification_inscription_a_valider' => 'New registration to be validated',
+	'titre_page_configurer_valider_inscription' => 'Validate Registration',
+
+	// V
+	'valider_inscription_titre' => 'Validate Registration',
+	'valider_membre' => 'Validate the registration of this author',
+);
